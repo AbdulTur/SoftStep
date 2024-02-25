21a8d67fe4a208391cba923f56b75daf832c33fb
AbdulTur <aturonov@mun.ca>
2024-02-25 03:00:16 -0330

    Created the Profile Screen UI

---
+++ app/src/main/java/com/softstep/softstep/ProfileActivity.java
@@ -0,0 +1,11 @@
+package com.softstep.softstep;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile); // Ensure to create this layout in res/layout
+    }
+}
