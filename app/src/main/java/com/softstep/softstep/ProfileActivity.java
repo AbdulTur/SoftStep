292488d1f2a6ebc5b576d7f835bc84465987c437
Léo Gilbert <lgilbert@mun.ca>
2024-03-12 23:51:19 -0230

    Checkbox event handlers working pt 2

--- app/src/main/java/com/softstep/softstep/ProfileActivity.java
+++ app/src/main/java/com/softstep/softstep/ProfileActivity.java
@@ -25,6 +25,7 @@ public class ProfileActivity extends AppCompatActivity {
 
         profileJsonHandler.context = getBaseContext();
         profileData = profileJsonHandler.loadProfile(getBaseContext());
+        System.out.println(profileData.StageOfParkinsons);
 
         RadioGroup stageOfParkinsonsGroup = findViewById(R.id.stageOfParkinsonsGroup);
         CheckBox checkBoxTremors = findViewById(R.id.checkBoxTremors);
@@ -50,103 +51,47 @@ public class ProfileActivity extends AppCompatActivity {
         RadioButton radioButton = findViewById(profileData.StageOfParkinsons);
         radioButton.setChecked(true);
 
-        stageOfParkinsonsGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup stageOfParkinsonsGroup, int i) {
-                RadioButton rb = findViewById(i);
-                profileData.StageOfParkinsons = stageOfParkinsonsGroup.getCheckedRadioButtonId();
-                System.out.println(profileData.StageOfParkinsons);
-                profileJsonHandler.saveProfile(profileData);
-                Toast.makeText(getApplicationContext(), String.valueOf(stageOfParkinsonsGroup.getCheckedRadioButtonId()), Toast.LENGTH_SHORT).show();
-            } 
+        stageOfParkinsonsGroup.setOnCheckedChangeListener((stageOfParkinsonsGroup1, i) -> {
+            RadioButton rb = findViewById(i);
+            profileData.StageOfParkinsons = stageOfParkinsonsGroup1.getCheckedRadioButtonId();
+            System.out.println(profileData.StageOfParkinsons);
+            profileJsonHandler.saveProfile(profileData);
+            Toast.makeText(getApplicationContext(), String.valueOf(stageOfParkinsonsGroup1.getCheckedRadioButtonId()), Toast.LENGTH_SHORT).show();
         });
 
-        checkBoxTremors.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-                profileData.PrimaryPhysicalSymptoms[0] = (isChecked) ? checkBoxTremors.getId() : 1;
-                System.out.println(profileData.PrimaryPhysicalSymptoms[0]);
-            }
+        checkBoxTremors.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            profileData.PrimaryPhysicalSymptoms[0] = (isChecked) ? checkBoxTremors.getId() : 1;
+            System.out.println(profileData.PrimaryPhysicalSymptoms[0]);
         });
-        checkBoxBradykinesia.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-                profileData.PrimaryPhysicalSymptoms[1] = (isChecked) ?  checkBoxBradykinesia.getId() : 1;
-                System.out.println(profileData.PrimaryPhysicalSymptoms[1]);
-            }
+        checkBoxBradykinesia.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            profileData.PrimaryPhysicalSymptoms[1] = (isChecked) ?  checkBoxBradykinesia.getId() : 1;
+            System.out.println(profileData.PrimaryPhysicalSymptoms[1]);
         });
-        checkBoxRigidity.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-                profileData.PrimaryPhysicalSymptoms[2] = (isChecked) ?  checkBoxRigidity.getId() : 1;
-            }
-        });
-        checkBoxPosturalInstability.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-                profileData.PrimaryPhysicalSymptoms[3] = (isChecked) ?  checkBoxPosturalInstability.getId() : 1;
-            }
-        });
+        checkBoxRigidity.setOnCheckedChangeListener((buttonView, isChecked) -> profileData.PrimaryPhysicalSymptoms[2] = (isChecked) ?  checkBoxRigidity.getId() : 1);
+        checkBoxPosturalInstability.setOnCheckedChangeListener((buttonView, isChecked) -> profileData.PrimaryPhysicalSymptoms[3] = (isChecked) ?  checkBoxPosturalInstability.getId() : 1);
+
         //Areas Most affected event handlers
-        checkBoxHands.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-                profileData.AreasMostAffected[0] = (isChecked) ?  checkBoxHands.getId() : 1;
-            }
+        checkBoxHands.setOnCheckedChangeListener((buttonView, isChecked) -> profileData.AreasMostAffected[0] = (isChecked) ?  checkBoxHands.getId() : 1);
+        checkBoxArms.setOnCheckedChangeListener((buttonView, isChecked) -> profileData.AreasMostAffected[1] = (isChecked) ?  checkBoxArms.getId() : 1);
+        checkBoxLegs.setOnCheckedChangeListener((buttonView, isChecked) -> profileData.AreasMostAffected[2] = (isChecked) ?  checkBoxLegs.getId() : 1);
+        checkBoxNeck.setOnCheckedChangeListener((buttonView, isChecked) -> profileData.AreasMostAffected[3] = (isChecked) ?  checkBoxNeck.getId() : 1);
+        checkBoxFace.setOnCheckedChangeListener((buttonView, isChecked) -> profileData.AreasMostAffected[4] = (isChecked) ?  checkBoxFace.getId() : 1);
+
+        //Mobility Level Radio group event handler
+        mobilityLevelGroup.setOnCheckedChangeListener((mobilityLevelGroup1, i) -> {
+            profileData.StageOfParkinsons = mobilityLevelGroup1.getCheckedRadioButtonId();
+            profileJsonHandler.saveProfile(profileData);
+            Toast.makeText(getApplicationContext(), String.valueOf(mobilityLevelGroup1.getCheckedRadioButtonId()), Toast.LENGTH_LONG).show();
         });
-        checkBoxArms.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-                profileData.AreasMostAffected[1] = (isChecked) ?  checkBoxArms.getId() : 1;
-            }
-        });
-        checkBoxLegs.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-                profileData.AreasMostAffected[2] = (isChecked) ?  checkBoxLegs.getId() : 1;
-            }
-        });
-        checkBoxNeck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-                profileData.AreasMostAffected[3] = (isChecked) ?  checkBoxNeck.getId() : 1;
-            }
-        });
-        checkBoxFace.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-                profileData.AreasMostAffected[4] = (isChecked) ?  checkBoxFace.getId() : 1;
-            }
+
+        //Exercise history radio group event handler
+        exerciseHistoryGroup.setOnCheckedChangeListener((exerciseHistoryGroup1, i) -> {
+            profileData.StageOfParkinsons = exerciseHistoryGroup1.getCheckedRadioButtonId();
+            profileJsonHandler.saveProfile(profileData);
+
+            Toast.makeText(getApplicationContext(), String.valueOf(exerciseHistoryGroup1.getCheckedRadioButtonId()), Toast.LENGTH_LONG).show();
         });
         /*
-        //Mobility Level Radio group event handler
-        mobilityLevelGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup mobilityLevelGroup, int i) {
-                profileData.StageOfParkinsons = mobilityLevelGroup.getCheckedRadioButtonId();
-
-                ProfileData testpd = profileJsonHandler.loadProfile(getBaseContext());
-                System.out.println(testpd.MobilityLevel);
-                profileJsonHandler.context = getBaseContext();
-                profileJsonHandler.saveProfile(profileData);
-
-                Toast.makeText(getApplicationContext(), String.valueOf(mobilityLevelGroup.getCheckedRadioButtonId()), Toast.LENGTH_LONG).show();
-            }
-        });
-        //Exercise history radio group event handler
-        exerciseHistoryGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup exerciseHistoryGroup, int i) {
-                profileData.StageOfParkinsons = exerciseHistoryGroup.getCheckedRadioButtonId();
-
-                ProfileData testpd = profileJsonHandler.loadProfile(getBaseContext());
-                System.out.println(testpd.MobilityLevel);
-                profileJsonHandler.context = getBaseContext();
-                profileJsonHandler.saveProfile(profileData);
-
-                Toast.makeText(getApplicationContext(), String.valueOf(exerciseHistoryGroup.getCheckedRadioButtonId()), Toast.LENGTH_LONG).show();
-            }
-        });
         checkBoxNoLimitations.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
