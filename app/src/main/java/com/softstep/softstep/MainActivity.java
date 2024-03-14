5b76d3174a3d88f90e01bd5bceb6c2e041762353
AbdulTur <aturonov@mun.ca>
2024-03-13 22:04:10 -0230

    Added exercises buttons, profile buttons and navigation

--- app/src/main/java/com/softstep/softstep/MainActivity.java
+++ app/src/main/java/com/softstep/softstep/MainActivity.java
@@ -2,7 +2,11 @@ package com.softstep.softstep;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton profileButton = findViewById(R.id.profileButton);
+
+        profileButton.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+            Toast.makeText(getApplicationContext(), "Profile Button was clicked", Toast.LENGTH_SHORT).show();
+        });
+
     }
 }
